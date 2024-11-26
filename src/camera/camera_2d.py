49bfd233c87238cb1e3bf1cbc31c9cdb44beb94a
Дудкин Илья Андреевич <dudkin.i@milandr.ru>
2024-11-26 13:01:42 +0300

    start rewrite lab using sdl python wrapper pysdl

---
+++ src/camera/camera_2d.py
@@ -0,0 +1,78 @@
+from shape.point import *
+
+
+class Camera2D:
+    def __init__(self, width = 0, height = 0):
+        self.position = Point(0, 0)
+        self.zoom = 1.
+        self.width = width
+        self.height = height
+        self.update_aspect_ratio()
+
+    def update_aspect_ratio(self):
+        self.aspect_ratio = self.width / self.height
+
+
+    def normalize_coordinate(self, value, is_x):
+        if is_x:
+            return value * min(1., 1. / self.aspect_ratio)
+        else:
+            return value * min(1., self.aspect_ratio)
+
+
+    def denormalize_coordinate(self, value, is_x):
+        if is_x:
+            return value / min(1., 1. / self.aspect_ratio)
+        else:
+            return value / min(1., self.aspect_ratio)
+
+
+    def move(self, dx, dy):
+        normalized_dx = dx * 2. / self.width / self.zoom
+        normalized_dy = dy * 2. /self.height / self.zoom
+
+        self.position.x += self.normalize_coordinate(normalized_dx, True)
+        self.position.y += self.normalize_coordinate(normalized_dy, False)
+
+
+    def set_position(self, x, y):
+        self.position.x = self.normalize_coordinate(x, True)
+        self.position.y = self.normalize_coordinate(y, False)
+
+
+    def update_screen_size(self, width, height):
+        old_aspect = self.aspect_ratio
+
+        self.width = width
+        self.height = height
+
+        self.update_aspect_ratio()
+
+        if(old_aspect != self.aspect_ratio):
+            self.position.x = self.normalize_coordinate(
+                self.denormalize_coordinate(self.position.x, True), True
+            )
+
+            self.position.y = self.normalize_coordinate(
+                self.denormalize_coordinate(self.position.y, False), False
+            )
+
+
+    def world_to_screen(self, world_x, world_y, screen_x, screen_y):
+        normalized_x = self.normalize_coordinate(world_x - self.position.x, True) * zoom
+        normalized_y = self.normalize_coordinate(world_y - self.position.y, False) * zoom
+
+        screen_x = int((normalized_x + 1.) * self.width * 0.5)
+        screen_y = int((normalized_y + 1.) * self.height * 0.5)
+
+
+    def screen_to_world(self, screen_x, screen_y, world_x, world_y):
+        normalized_x = (screen_x * 2. / self.width - 1.)
+        normalized_y = (screen_y * 2. / self.height - 1.)
+
+        normalized_x = self.denormalize_coordinate(normalized_x / self.zoom, True)
+        normalized_y = self.denormalize_coordinate(normalized_y / self.zoom, False)
+
+
+        world_x = normalized_x + self.position.x
+        world_y = normalized_y + self.position.y
--- src/main.py
+++ src/main.py
@@ -1,61 +1,13 @@
 import sys
-from math import sin, tan
-
-from PyQt5.QtWidgets import *
-from PyQt5.QtCore import *
-from PyQt5.QtGui import *
-
-from model.line import Line
-from model.matrix2d import Matrix2D
-from model.figure import Figure
-from model.closed_figure import ClosedFigure
-
-from camera.camera2d import Camera2D
-
-from view.view import ZoomableGraphicsView
-from scene.scene import Scene
-
-import numpy as np
-
-
-class Function:
-    def __call__(self, func, x):
-        return [func(item) for item in x]
-
-
-class MainWindow(QMainWindow):
-    def __init__(self):
-        super().__init__()
-
-        self.view = ZoomableGraphicsView()
-        self.scene = Scene()
-
-        x = np.linspace(0, 100, 1000)
-
-        y = np.sin(x)
-
-        matrix = Matrix2D()
-        for i in range(len(x)):
-            matrix_data = [x[i], y[i]]
-            matrix.data.append(matrix_data)
-
-        figure = Figure(matrix)
-
-        figure.create_lines()
-
-        self.scene.scene_controller.add_figure(figure)
-
-        self.view.setScene(self.scene)
-
-        self.camera = Camera2D(self)
-
-        self.view.show()
+import sdl2.ext
 
 
 if __name__ == "__main__":
-    app = QApplication(sys.argv)
+    sdl2.ext.init()
 
-    window = MainWindow()
+    window = sdl2.ext.Window("Computer Graphics", size = (640, 480))
+    window.show()
 
-    app.exec_()
-
+    processor = sdl2.ext.TestEventProcessor()
+    processor.run(window)
+    sdl2.ext.quit()
